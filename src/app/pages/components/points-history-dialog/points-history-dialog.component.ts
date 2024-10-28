15ad0214add7342c7db931ac0085a9bf14632b7e
Gabriel Alves <ga654066@gmail.com>
2024-10-28 00:46:38 -0300

    historico de pontos concluido

--- src/app/pages/components/points-history-dialog/points-history-dialog.component.ts
+++ src/app/pages/components/points-history-dialog/points-history-dialog.component.ts
@@ -1,19 +1,26 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { PointsHistoryDialogService } from './points-history-dialog.service';
+import { MatCommonModule } from '@angular/material/core';
+import { CommonModule } from '@angular/common';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-points-history-dialog',
   standalone: true,
   imports: [
-
+    MatCommonModule,
+    CommonModule,
+    MatDialogModule,
+    MatButton
   ],
   templateUrl: './points-history-dialog.component.html',
   styleUrl: './points-history-dialog.component.css'
 })
 export class PointsHistoryDialogComponent {
   public userId : any = null;
-  public pointsHistory: any[] = [];
+  public pointsHistory:  any[] = [];
 
   constructor(
     public dialogRef: MatDialogRef<PointsHistoryDialogComponent>,
